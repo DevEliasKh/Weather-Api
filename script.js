e07f52ee7d3ee56f2668ab1d220889b7583c8834
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2023-07-30 10:53:59 +0330

    make api work and code JS

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// api key abe35ea518404fca9b1122150232907
+const searchBtn = document.querySelector('button');
+let cityInput = '';
+let temp = '';
+let cityName = '';
+let condition = '';
+let weatherImgSrc = '';
+searchBtn.addEventListener('click', () => {
+	getUserValue();
+	showWeather();
+});
+
+function getUserValue() {
+	const searchBar = document.querySelector('input').value;
+	// check for just letter and nothing else
+	if (!searchBar) {
+		console.log('no value');
+	} else {
+		cityInput = searchBar;
+		console.log(cityInput);
+	}
+}
+async function showWeather() {
+	const response = await fetch(
+		`http://api.weatherapi.com/v1/current.json?key=abe35ea518404fca9b1122150232907&q=${cityInput}&aqi=no`,
+		{ mode: 'cors' }
+	);
+	const current = response.json().then(function (response) {
+		temp = response.current.feelslike_c;
+		condition = response.current.condition.text;
+		weatherImgSrc = response.current.condition.icon;
+		cityName = response.location.name + ' ' + response.location.country;
+		DOM();
+	});
+	function DOM() {
+		const weatherImg = document.querySelector('img');
+		const location = document.querySelector('.city-name');
+		const cityTemp = document.querySelector('.temp');
+		const cityCondition = document.querySelector('.condition');
+		weatherImg.src = weatherImgSrc;
+		location.innerHTML = `<span class="fas fa-map-marker-alt"> </span> ${cityName}`;
+		cityTemp.innerText = temp;
+		cityCondition.innerText = condition;
+	}
+}
